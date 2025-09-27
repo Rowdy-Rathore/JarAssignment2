72560070e6119f0c5cd496909a98bb283bf097c1
Rowdy-Rathore <hrithikrathore786@gmail.com>
2025-09-27 09:52:49 +0530

    Main Activity Optimized and refactored

--- app/src/main/java/com/example/jarassignment/MainActivity.kt
+++ app/src/main/java/com/example/jarassignment/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.jarassignment
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -16,25 +15,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.jarassignment.ui.EducationScreen
 import com.example.jarassignment.ui.navigation.EducationNavHost
-import com.example.jarassignment.ui.screens.IntroScreen
 import com.example.jarassignment.ui.theme.JarAssignmentTheme
-import dagger.hilt.android.AndroidEntryPoint
 import com.example.jarassignment.ui.viewmodel.EducationViewModel
 import com.example.jarassignment.util.UiState
+import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -49,19 +40,18 @@ class MainActivity : ComponentActivity() {
             JarAssignmentTheme {
                 val navController = rememberNavController()
 
-                // Start loading once when composable is first displayed
-//                LaunchedEffect(Unit) {
-//                    viewModel.loadEducationMetadata()
-//                }
-
                 val uiState by viewModel.uiState.collectAsState()
 
                 when (uiState) {
                     is UiState.Idle, UiState.Loading -> {
-                        Box(modifier = Modifier.fillMaxSize(), contentAlignment = androidx.compose.ui.Alignment.Center) {
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = androidx.compose.ui.Alignment.Center
+                        ) {
                             CircularProgressIndicator()
                         }
                     }
+
                     is UiState.Error -> {
                         val err = uiState as UiState.Error
                         Column(
@@ -77,44 +67,16 @@ class MainActivity : ComponentActivity() {
                             }
                         }
                     }
+
                     is UiState.Success -> {
                         val data = (uiState as UiState.Success).data.data.manualBuyEducationData
-//                        IntroScreen(
-//                            introTitle = data.introTitle ?: "",
-//                            introSubtitle = data.introSubtitle ?: "",
-//                            onFinished = { onIntroFinished(data) }
-//                        )
-
-
-/*                        EducationScreen(
-                            viewModel = viewModel,
-                            onIntroFinished = { data ->
-                                navController.navigate("education_screen")
-                            }
-                        )*/
-
                         EducationNavHost(
                             navController = navController,
                             educationData = data,
                             onExitFlow = { finish() } // exit or navigate elsewhere
                         )
-//            val cardList = data.data.manualBuyEducationData.educationCardList
-//            EducationCardList(cardList = cardList)
                     }
                 }
-
-                // Observe ViewModel data
-//                val educationResponse by viewModel.educationData.observeAsState()
-
-//                educationResponse?.let { response ->
-//                    val educationData = response.data.manualBuyEducationData
-//
-//                    EducationNavHost(
-//                        navController = navController,
-//                        educationData = educationData,
-//                        onExitFlow = { finish() } // exit or navigate elsewhere
-//                    )
-//                }
             }
         }
 
